4ce9730e32b98030912e48456d2533f79bba5390
jyothsna kopera <jyothsnakopera@gmail.com>
2026-01-16 18:36:37 +0530

    Renaming Frontend to temp

--- frontend/src/pages/StudentDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StudentDashboard() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Student Dashboard</h1>
-      <p>UI working correctly</p>
-    </div>
-  );
-}
